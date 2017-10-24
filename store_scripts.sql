74726aa1eb4de99264094874ed24bb6197db5e90
sarahama <sam8bz@virginia.edu>
2017-10-23 21:48:19 -0400

    all table scripts now in store_scripts file

--- store_scripts.sql
+++ store_scripts.sql
@@ -42,3 +42,56 @@ CREATE TABLE `Feedback` (
  `Submitted_On` datetime NOT NULL,
  PRIMARY KEY (`Feedback_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1
+
+CREATE TABLE `User` (
+ `User_ID` int(11) NOT NULL AUTO_INCREMENT,
+ `FB_ID` varchar(100) DEFAULT NULL,
+ `First_Name` varchar(100) NOT NULL,
+ `Last_Name` varchar(100) DEFAULT NULL,
+ `Date_Joined` datetime NOT NULL,
+ `Last_Login` datetime NOT NULL,
+ `Access_Token` varchar(200) DEFAULT NULL,
+ `School` varchar(100) DEFAULT NULL,
+ `Current_Points` int(11) NOT NULL DEFAULT '0',
+ `Total_Points` int(11) NOT NULL DEFAULT '0',
+ `On_Leader_Board` bit(1) DEFAULT NULL,
+ `Email` varchar(100) NOT NULL,
+ `Password` varchar(300) NOT NULL,
+ `Profile_Path` blob NOT NULL,
+ `Phone` varchar(16) NOT NULL,
+ PRIMARY KEY (`User_ID`),
+ UNIQUE KEY `Access_Token` (`Access_Token`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1
+
+CREATE TABLE `Event` (
+ `Event_ID` int(11) NOT NULL AUTO_INCREMENT,
+ `Event_Start_Date` datetime NOT NULL,
+ `Event_Name` varchar(100) NOT NULL,
+ `Event_Details` varchar(200) NOT NULL,
+ `Event_Color` varchar(20) NOT NULL,
+ `Event_Organization` int(11) NOT NULL,
+ `Event_Points` int(11) NOT NULL,
+ `Event_Code` varchar(200) NOT NULL,
+ `Event_Address` varchar(100) NOT NULL,
+ `Event_City` varchar(100) NOT NULL,
+ `Event_State` varchar(2) NOT NULL,
+ `Event_Zip` int(11) NOT NULL,
+ `Event_End_Date` datetime NOT NULL,
+ PRIMARY KEY (`Event_ID`),
+ UNIQUE KEY `Event_ID` (`Event_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
+
+CREATE TABLE `Event_Attendance` (
+ `Attendance_ID` int(11) NOT NULL AUTO_INCREMENT,
+ `User_ID` int(11) NOT NULL,
+ `Event_ID` int(11) NOT NULL,
+ PRIMARY KEY (`Attendance_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
+
+CREATE TABLE `Event_Interest` (
+ `Interest_ID` int(11) NOT NULL AUTO_INCREMENT,
+ `Event_ID` int(11) NOT NULL,
+ `User_ID` int(11) NOT NULL,
+ PRIMARY KEY (`Interest_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1
+
